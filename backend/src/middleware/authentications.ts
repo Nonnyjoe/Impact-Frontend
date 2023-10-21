c3dd280b25d538dc835a7863be090442ccd9bb18
Samson Ajulor <samsonajulor@gmail.com>
2023-10-21 23:44:19 +0100

    added auth roles

--- backend/src/middleware/authentications.ts
+++ backend/src/middleware/authentications.ts
@@ -4,6 +4,8 @@ import User from '../models/user';
 import { ResponseCode } from '../@types';
 import { ApiError, StatusCode, Toolbox } from '../utils';
 
+const { verifyToken } = Toolbox;
+
 const Authentications = {
   async authenticate(req: Request, res: Response, next: NextFunction) {
     try {
@@ -23,8 +25,8 @@ const Authentications = {
           'authenticate',
           StatusCode.UNAUTHORIZED
         );
-      const decoded: any = jwt.verify(tokenString, process.env.JWT_SECRET as string);
-      const user = await User.findById(decoded?.id).exec();
+      const decoded: any = await verifyToken(tokenString);
+      const user = await User.findOne({email: decoded}).exec();
 
       if (!decoded || !user)
         throw new ApiError(
@@ -43,6 +45,35 @@ const Authentications = {
       });
     }
   },
+
+  async authorize(roles: string[]) {
+    return (req: Request, res: Response, next: NextFunction) => {
+      try {
+        const user = res.locals.user;
+        if (!user)
+          throw new ApiError(
+            'Authentications',
+            'Not authorized',
+            'authorizeRoles',
+            StatusCode.UNAUTHORIZED
+          );
+        if (!roles.includes(user.role))
+          throw new ApiError(
+            'Authentications',
+            'Not authorized',
+            'authorizeRoles',
+            StatusCode.UNAUTHORIZED
+          );
+        next();
+      } catch (error: any) {
+        return res.status(error.statusCode || StatusCode.INTERNAL_SERVER_ERROR).json({
+          status: !!ResponseCode.FAILURE,
+          message: error.message,
+          data: null,
+        });
+      }
+    };
+  }
 };
 
 export default Authentications;
