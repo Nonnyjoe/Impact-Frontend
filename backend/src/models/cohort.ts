3e058bb7eda385dae8301d7d731e1b7a433704b5
MayowaObisesan <mayowaobi74@gmail.com>
2023-10-22 19:52:51 +0100

    Update cohort files

---
+++ backend/src/models/cohort.ts
@@ -0,0 +1,18 @@
+import { number } from "joi";
+import mongoose from "mongoose";
+
+const CohortSchema = new mongoose.Schema(
+    {
+        alias: {
+            type: Number
+        },
+        isActive: {
+            type: Boolean,
+            default: false,
+            required: [true, "Specify active status"]
+        },
+    },
+    { toJSON: { virtuals: true }, toObject: { virtuals: true }, timestamps: true }
+);
+
+export default mongoose.model('Cohort', CohortSchema);
