0df29b247a3c8b855b13fb606f8aa8749aa0392a
James Osunkiyesi <jaydhales@gmail.com>
2023-12-04 15:51:35 +0100

    deps

--- frontend-ts/src/pages/onboard/[token]/index.tsx
+++ frontend-ts/src/pages/onboard/[token]/index.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import Image from 'next/image';
 import React, { useEffect, useState } from 'react';
 import w3bLogo from '@/assets/Images/Logo.png';
@@ -43,7 +44,7 @@ const CreateUser = () => {
         email: (router.query.email as string) ?? '',
       });
     }
-  }, [router, info]);
+  }, [router]);
 
   const handleSubmit = async () => {
     try {
